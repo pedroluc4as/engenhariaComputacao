3c19a603be98953e2e2d903373d1e239f4bb32a4
pedroluc4as <ordepsperandio@hotmail.com>
2025-09-09 14:57:07 -0300

    a

---
+++ POO/ex1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Livro {
+    // modificadores de acesso -> nível de visibilidade dos atributos e métodos
+    private:
+    //private -> visivel class
+    //quando os atributos (atr ou att) estão privados damos o nome de encapsulamento 
+
+    string isbn, titulo, autor;
+
+    public:
+
+
+};
+
+class Aluno {};
+
+
+
+class Emprestimo {};
