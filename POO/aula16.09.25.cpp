72e0509d99a1a1db94a04289b1250061acf6825b
pedroluc4as <ordepsperandio@hotmail.com>
2025-09-16 15:54:14 -0300

    ok

--- POO/aula16.09.25.cpp
+++ POO/aula16.09.25.cpp
@@ -3,34 +3,45 @@
 
 using namespace std;
 
-class Aluno
+class Disciplina
 {
 private:
-    string cpf, nome, ra, dt_nasc;
-
+    string cod, nome, professor;
 public:
-
-};
-
-class Matricula
-{
-private:
-
-public:
+    Disciplina(string cod, string nome, string professor) 
+    : cod(std::move(cod)), nome(std::move(nome)), professor(std::move(professor)) {}
 };
 
 class Notas
 {
 private:
+    float nota;
+    Disciplina *disciplina;
 
 public:
-
-
+    Notas(float nota, Disciplina *disciplina) : nota(std::move(nota)), disciplina(std::move(disciplina)) {}
 };
 
-class Disciplina
+class Aluno
 {
-public:
-
 private:
+    string cpf, nome, ra, dt_nasc;
+    Notas *notas;
+public:
+    Aluno(string cpf, string nome, string ra, string dt_nasc, Notas *notas) 
+    : cpf(std::move(cpf)), nome(std::move(nome)), ra(std::move(ra)), 
+    dt_nasc(std::move(dt_nasc)), notas(std::move(notas)) {}
+
+    //metodos
+   int media();
+};
+
+class Matricula
+{
+private:
+    string num;
+    Aluno *aluno;
+
+public:
+    Matricula(string num, Aluno *aluno) : num(std::move(num)), aluno(std::move(aluno)) {}
 };
