0339a0533c784ca1251345966cd26689a5620763
pedroluc4as <ordepsperandio@hotmail.com>
2025-09-16 15:18:08 -0300

    simmm

---
+++ POO/aula16.09.25.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Aluno
+{
+private:
+    string cpf, nome, ra, dt_nasc;
+
+public:
+
+};
+
+class Matricula
+{
+private:
+
+public:
+};
+
+class Notas
+{
+private:
+
+public:
+
+
+};
+
+class Disciplina
+{
+public:
+
+private:
+};
