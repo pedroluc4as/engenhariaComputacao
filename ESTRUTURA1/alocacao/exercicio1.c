83c0479264ce1c6686c52b0dd6caf285bffefacf
pedroluc4as <ordepsperandio@hotmail.com>
2025-09-25 09:23:30 -0300

    la

---
+++ ESTRUTURA1/alocacao/exercicio1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define tamnho 10000000
+int main()
+{
+    int *inteiros;
+    int cont = 0;
+
+    inteiros = malloc (10000000 * sizeof(int));
+
+    inteiros[1] = 3;
+
+    for(int i = 0; i < tamnho; i++)
+    {
+        if(inteiros[i] == 0)
+        {
+            cont++;
+        }
+    }
+    printf("%d\n",cont);
+
+    free(inteiros);
+    
+    return 0;
+}
